a0dbc2461a82c7d21517ee5fe7ce8433cc1fc4f6
kimiyash <kimiyash@gmail.com>
2024-10-22 17:55:41 +0900

    commit

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,23 +1,41 @@
 use std::sync::Arc;
 
-use adapter::repository::book::BookRespositoryImpl;
-use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
-use kernel::repository::book::BookRespository;
-use kernel::repository::health::HealthCheckRepository;
+use adapter::{
+    database::{model::auth, ConnectionPool},
+    redis::RedisClient,
+    repository::{
+        auth::AuthRepositoryImpl, book::BookRespositoryImpl, health::HealthCheckRepositoryImpl,
+    },
+};
+use kernel::repository::{
+    auth::AuthRepository, book::BookRespository, health::HealthCheckRepository,
+};
+use shared::config::AppConfig;
 
 #[derive(Clone)]
 pub struct AppRegistry {
     health_check_repository: Arc<dyn HealthCheckRepository>,
     book_repository: Arc<dyn BookRespository>,
+    auth_repository: Arc<dyn AuthRepository>,
 }
 
 impl AppRegistry {
-    pub fn new(pool: ConnectionPool) -> Self {
+    pub fn new(
+        pool: ConnectionPool,
+        redis_client: Arc<RedisClient>,
+        app_config: AppConfig,
+    ) -> Self {
         let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
         let book_repository = Arc::new(BookRespositoryImpl::new(pool.clone()));
+        let auth_repository = Arc::new(AuthRepositoryImpl::new(
+            pool.clone(),
+            redis_client.clone(),
+            app_config.auth.ttl,
+        ));
         Self {
             health_check_repository,
             book_repository,
+            auth_repository,
         }
     }
 
@@ -27,5 +45,9 @@ impl AppRegistry {
 
     pub fn book_repository(&self) -> Arc<dyn BookRespository> {
         self.book_repository.clone()
+    }
+
+    pub fn auth_repository(&self) -> Arc<dyn AuthRepository> {
+        self.auth_repository.clone()
     }
 }
--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,9 +1,12 @@
-use std::net::{Ipv4Addr, SocketAddr};
+use std::{
+    net::{Ipv4Addr, SocketAddr},
+    sync::Arc,
+};
 
-use adapter::database::connect_database_with;
+use adapter::{database::connect_database_with, redis::RedisClient};
 use anyhow::Context;
 use anyhow::Result;
-use api::route::{book::build_book_routers, health::build_health_check_routers};
+use api::route::{auth, book::build_book_routers, health::build_health_check_routers};
 use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
@@ -26,8 +29,9 @@ async fn main() -> Result<()> {
 async fn bootstrap() -> Result<()> {
     let app_config = AppConfig::new()?;
     let pool = connect_database_with(&app_config.database);
+    let kv = Arc::new(RedisClient::new(&app_config.redis)?);
 
-    let registry = AppRegistry::new(pool);
+    let registry = AppRegistry::new(pool, kv, app_config);
 
     let cors = CorsLayer::new()
         .allow_origin(Any)
@@ -37,6 +41,7 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routers())
         .merge(build_book_routers())
+        .merge(auth::routes())
         .layer(cors)
         .layer(
             TraceLayer::new_for_http()
