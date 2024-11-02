d828dcc3e010af28183c83e23cd61d4f6366080e
kimiyash <kimiyash@gmail.com>
2024-11-02 14:55:30 +0900

    commit

--- rstest-example/src/lib.rs
+++ rstest-example/src/lib.rs
@@ -5,12 +5,11 @@ pub fn sub(a: i32, b: i32) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_sub_1() {
-        assert_eq!(sub(10, 0), 10);
-    }
-    #[test]
-    fn test_sub_2() {
-        assert_eq!(sub(100, 5), 95);
+    use rstest::rstest;
+    #[rstest]
+    #[case(10, 0, 10)]
+    #[case(100, 5, 95)]
+    fn test_sub_1(#[case] a: i32, #[case] b: i32, #[case] expected: i32) {
+        assert_eq!(sub(a, b), expected);
     }
 }
