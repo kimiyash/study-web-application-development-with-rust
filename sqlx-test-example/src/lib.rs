ad3380318cba319f3129044a20c152c47ec190d8
kimiyash <kimiyash@gmail.com>
2024-11-02 15:41:28 +0900

    commit

--- sqlx-test-example/src/lib.rs
+++ sqlx-test-example/src/lib.rs
@@ -11,14 +11,14 @@ mod tests {
         assert_eq!(result, Some(2))
     }
 
-    #[sqlx::test]
-    async fn it_works(pool: sqlx::PgPool) {
+    #[sqlx::test(fixtures("common"))]
+    async fn it_works2(pool: sqlx::PgPool) {
         // 接続確認
-        let row = sqlx::query!("SELECT 1 + 1 AS result")
+        let row = sqlx::query!("SELECT author FROM books WHERE title = 'Test Book 1'")
             .fetch_one(&pool)
             .await
             .unwrap();
-        let result = row.result;
-        assert_eq!(result, Some(2))
+        let result = row.author;
+        assert_eq!(result, "Test Author 1".to_string());
     }
 }
